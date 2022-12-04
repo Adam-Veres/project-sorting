1f46a623d5f82481ff6960c008996bb18a42453c
Yurii Plaksenkov <yurii_plaksenkov@epam.com>
2022-12-04 03:16:02 +0200

    Fix cors

--- src/main/java/com/epam/config/WebSecurityConfig.java
+++ src/main/java/com/epam/config/WebSecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -77,7 +79,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		CorsConfiguration corsConfiguration =new CorsConfiguration();
+		corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", corsConfiguration.applyPermitDefaultValues());
 		return source;
 	}
 
