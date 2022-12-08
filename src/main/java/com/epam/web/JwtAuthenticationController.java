1dd0ccda2792c0c8ec8f1a4736412f4fa2d8b101
Yurii Plaksenkov <yurii_plaksenkov@epam.com>
2022-12-08 22:17:36 +0200

    Small refactoring. Remove asterisks

--- src/main/java/com/epam/web/JwtAuthenticationController.java
+++ src/main/java/com/epam/web/JwtAuthenticationController.java
@@ -9,7 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/auth/")
